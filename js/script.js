3024c7ca3394a8b3a23f2a4c14831d65fa48dcdf
Artem Rybalkin <artem.rybalkin86@gmail.com>
2016-07-30 22:21:45 +0300

    added check

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-var base = prompt( 'input base' ); //запрашиваем базовое число
-var exponent = prompt( 'input exponent' ); //запрашиваем экспоненту
+var base;
+var exponent;
 var result = 1;
+
+base = prompt( 'input base' );
+
+if ( isNaN (base) || base <= 0 ) {
+	do {
+ 		base = prompt( 'please input positive number' );
+ 	 } while ( isNaN (base) || base <= 0 )
+}
+
+exponent = prompt( 'input exponent' );
+
+if ( isNaN (exponent) || exponent <= 0 ) {
+	do {
+ 		exponent = prompt( 'please input positive number' );
+ 	 } while ( isNaN (exponent) || exponent <= 0 )
+}
 
 function pow(base, exponent) {
 
